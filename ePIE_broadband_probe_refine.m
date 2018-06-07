b91764214011728e6b7722f7e8b48feefcb054a4
arana89 <arjun.rana@gmail.com>
2018-06-07 12:47:40 -0700

    changed output filename

--- ePIE_broadband_probe_refine.m
+++ ePIE_broadband_probe_refine.m
@@ -28,7 +28,7 @@ job_ID = job_ID(~isspace(job_ID));
 nModes = length(pixel_size);
 central_mode = ePIE_inputs.central_mode; %best mode for probe replacement
 fresnel_dist = ePIE_inputs.fresnel_dist; %probe to sample
-filename = strcat('reconstruction_',filename,'_',job_ID);
+filename = strcat('reconstruction_probe_replace_',filename,'_',job_ID);
 filename = strrep(filename,'__','_');
 %% parameter inputs
 if isfield(ePIE_inputs, 'saveOutput')
